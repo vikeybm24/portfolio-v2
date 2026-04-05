83a2d5230b37227ae557631dbde4a6080693adc5
vikeybm24 <vikeybm24@gmail.com>
2026-04-06 00:16:27 +0530

    feat: configure GitHub Actions deployment workflow and set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-v2/",
   server: {
     host: '0.0.0.0', // Make the server accessible from external devices
     port: 3000, // You can change the port if needed
